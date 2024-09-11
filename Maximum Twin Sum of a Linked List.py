9f6967c6bb8a0e7fd357d1b98bf9a7952f362893
Ashutosh Gaur <121347667+ashutoshgaur0809@users.noreply.github.com>
2024-09-11 18:10:37 +0530

    Create Maximum Twin Sum of a Linked List.py

---
+++ Maximum Twin Sum of a Linked List.py
@@ -0,0 +1,51 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        lst = []
+        temp = head
+        while temp:
+            lst.append(temp.val)
+            temp = temp.next
+        
+        sum = 0
+        l = 0
+        r = len(lst) - 1
+        while(l < r):
+            sump = lst[l] + lst[r]
+            if sump > sum:
+                sum = sump
+            
+            l += 1
+            r -= 1
+
+        return sum 
+
+        
+
+# Input: head = [5,4,2,1]
+# Output: 6
+# Explanation:
+# Nodes 0 and 1 are the twins of nodes 3 and 2, respectively. All have twin sum = 6.
+# There are no other nodes with twins in the linked list.
+# Thus, the maximum twin sum of the linked list is 6. 
+# Example 2:
+
+
+# Input: head = [4,2,2,3]
+# Output: 7
+# Explanation:
+# The nodes with twins present in this linked list are:
+# - Node 0 is the twin of node 3 having a twin sum of 4 + 3 = 7.
+# - Node 1 is the twin of node 2 having a twin sum of 2 + 2 = 4.
+# Thus, the maximum twin sum of the linked list is max(7, 4) = 7. 
+# Example 3:
+
+
+# Input: head = [1,100000]
+# Output: 100001
+# Explanation:
+# There is only one node with a twin in the linked list having twin sum of 1 + 100000 = 100001.
